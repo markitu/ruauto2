39b444508283a796c3af412c8b1b3d8c49d81af2
Igor Mashkov <igor.mashkov@gmail.com>
2020-05-20 22:02:29 +0300

    minor changes (Igor M)

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+
+public class Main extends MainBase {
+
+  @Test
+  public void mainPageTitle() throws InterruptedException {
+
+
+    openNewsWithExactNumber(3);
+
+
+
+
+
+  }
+
+
+}
